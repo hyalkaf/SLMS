6844ddd6f69f2c26890992010790781fcec40caa
namuthan <nabilbbb33@gmail.com>
2016-04-17 09:53:36 -0600

    Initial Commit

---
+++ SLMS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SLMS
+//
+//  Created by Nabil Ali Muthanna  on 2016-04-17.
+//  Copyright © 2016 Nabil Ali Muthanna . All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
